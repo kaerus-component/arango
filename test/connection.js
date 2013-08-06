bdb0958b35b4a5092212ddb743e447ed18f7afce
anders elo <anders@kaerus.com>
2013-08-06 15:49:19 +0200

    added Connection tests

---
+++ test/connection.js
@@ -0,0 +1,30 @@
+var should = require('should'),
+	arango = require('..'),
+	base64 = require('base64');
+
+describe("Connection",function(){
+	it('should have a Connection method',function(){
+		arango.should.have.ownProperty('Connection');
+	})
+
+	it('should have default server settings',function(){
+		var db = new arango.Connection;
+		db.should.have.property('server');
+		db.server.should.eql({protocol:"http",hostname:"127.0.0.1",port:8529});
+	})
+
+	it('should be able to parse a Connection string',function(){
+		var db = new arango.Connection("https://user:pass@hostname:8529/collection");
+
+		var headers = {authorization:'Basic ' + base64.encode(db.server.username + ':' + db.server.password) };
+
+		db.server.should.eql({
+			protocol:'https',
+			username:'user',
+			password:'pass',
+			hostname:'hostname',
+			port:8529,
+			headers: headers
+		})	
+	})
+})
