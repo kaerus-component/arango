1dd09cbd37bccddc5c2ef794157b66ee3cfb94e4
anders elo <anders@kaerus.com>
2013-08-07 23:54:23 +0200

    removed spooky use of params causing all kinds of trouble

--- test/collection.js
+++ test/collection.js
@@ -1,5 +1,6 @@
 try{ arango = require('arango') } catch (e){ arango = require('..') }
 
+function jerr(o){return JSON.stringify(o,null,2)}
 
 describe("collection",function(){
 	var db = new arango.Connection("http://127.0.0.1:8529");
@@ -13,34 +14,67 @@ describe("collection",function(){
 	})
 
 	describe("create",function(){
-		var collection = "test1";
+		var collection = "test1", 
+			options = {
+					journalSize: 10000000,
+					waitForSync:true,
+					keyOptions: { 
+						type: "autoincrement", 
+						increment: 5, 
+						allowUserKeys: true 
+					}
+			};
 
-		afterEach(function(done){
-			db.collection.delete(collection).then(function(res){
-				done();
-			},function(e){ throw e });
+		beforeEach(function(done){
+			db.collection.delete(collection,function(){ done() });
 		})
 
-		it('should be able to create collection',function(done){
-			db.collection.create(collection).then(function(res){
-				done();
-			},function(e){ throw e });
+		it('should be able to create a collection by name',function(done){
+			db.collection.create(collection,function(err,ret){
+				done(err?jerr(ret):err);
+			});
+		})
+
+		it('should be able to pass options',function(done){
+			db.collection.create(collection,options,function(err,ret){
+				done(err?jerr(ret):err);
+			});
 		})
 	})
 
-	describe("delete",function(){
+	describe("get",function(){
 		var collection = "test2";
 
 		beforeEach(function(done){
-			db.collection.create(collection).then(function(res){
-				done();
-			},function(e){ throw e });
+			db.collection.create(collection,{journalSize:10000000, waitForSync:true},function(err,ret){
+				done(); // may fail if there is a collection.
+			});
 		})
 
-		it('should be able to delete collection',function(done){
-			db.collection.delete(collection).then(function(res){
-				done();
-			},function(e){ throw e });
+		afterEach(function(done){
+			db.collection.delete(collection,function(err,ret){
+				done(); // may fail if there is a collection.
+			});
 		})
-	})	
+
+		describe('collection.get',function(done){
+
+			it('should be able to get by name',function(done){
+				db.collection.get(collection,function(err,ret){
+					done(err);
+				});
+			})
+
+			it('should be able to get by id',function(done){
+				db.collection.get(collection,function(err,ret){
+					if(err) done(err);
+
+					db.collection.get(ret.id,function(err,ret){
+						done(err);
+					});
+				});
+			})
+		})
+	})
+
 })
