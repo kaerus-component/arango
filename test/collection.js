f7d9d89c85988169b76f83a8f0142b4c2f660c31
Anders Elo <anders@kaerus.com>
2013-12-12 12:20:43 +0100

    added tests on CollectionAPI

--- test/collection.js
+++ test/collection.js
@@ -4,22 +4,15 @@ function jerr(o){return JSON.stringify(o,null,2)}
 
 describe("collection",function(){
 	var db = new arango.Connection("http://127.0.0.1:8529");
-	
-	describe("list",function(){	
-		it('should be able to list collections',function(done){
-			db.collection.list().then(function(res){
-				done();
-			},function(e){ throw e });
-		})
-	})
 
 	describe("create",function(){
 		var collection = "test1", 
 			options = {
-					journalSize: 10000000,
-					waitForSync:true,
+					journalSize: 12345678,
+					waitForSync: true,
 					keyOptions: { 
-						type: "autoincrement", 
+						type: "autoincrement",
+						offset: 0, 
 						increment: 5, 
 						allowUserKeys: true 
 					}
@@ -31,13 +24,24 @@ describe("collection",function(){
 
 		it('should be able to create a collection by name',function(done){
 			db.collection.create(collection,function(err,ret){
+				ret.isSystem.should.eql(false);
+				ret.status.should.eql(3);
+				ret.type.should.eql(2);
+				ret.isVolatile.should.eql(false);
+				ret.error.should.eql(false);
 				done(err?jerr(ret):err);
 			});
 		})
 
-		it('should be able to pass options',function(done){
+		it('should be able to pass options and getProperties',function(done){
 			db.collection.create(collection,options,function(err,ret){
-				done(err?jerr(ret):err);
+				ret.waitForSync.should.eql(options.waitForSync);
+				db.collection.getProperties(ret.id,function(err,prop){
+					/* note: rounded to KB */ 
+					(prop.journalSize >> 10).should.equal(options.journalSize >> 10); 
+					prop.keyOptions.should.eql(options.keyOptions);
+					done(err?jerr(ret):err);
+				})
 			});
 		})
 	})
@@ -47,29 +51,74 @@ describe("collection",function(){
 
 		beforeEach(function(done){
 			db.collection.create(collection,{journalSize:10000000, waitForSync:true},function(err,ret){
-				done(); // may fail if there is a collection.
+				done(err?jerr(ret):err); 
 			});
 		})
 
 		afterEach(function(done){
 			db.collection.delete(collection,function(err,ret){
-				done(); // may fail if there is a collection.
+				done(err?jerr(ret):err); 
 			});
 		})
 
-		describe('collection.get',function(done){
+		describe('collection get',function(done){
+			it('list',function(done){
+				db.collection.list().then(function(res){
+					done();
+				},function(e){ throw e });
+			})
 
-			it('should be able to get by name',function(done){
+			it('by name',function(done){
 				db.collection.get(collection,function(err,ret){
 					done(err);
 				});
 			})
 
-			it('should be able to get by id',function(done){
+			it('by id',function(done){
 				db.collection.get(collection,function(err,ret){
 					if(err) done(err);
 
 					db.collection.get(ret.id,function(err,ret){
+						done(err);
+					});
+				});
+			})
+
+			it('revision',function(done){
+				db.collection.get(collection,function(err,ret){
+					if(err) done(err);
+
+					db.collection.revision(ret.id,function(err,ret){
+						done(err);
+					});
+				});
+			})
+
+			it('count',function(done){
+				db.collection.get(collection,function(err,ret){
+					if(err) done(err);
+
+					db.collection.count(ret.id,function(err,ret){
+						done(err);
+					});
+				});
+			})
+
+			it('figures',function(done){
+				db.collection.get(collection,function(err,ret){
+					if(err) done(err);
+
+					db.collection.figures(ret.id,function(err,ret){
+						done(err);
+					});
+				});
+			})
+
+			it('properties',function(done){
+				db.collection.get(collection,function(err,ret){
+					if(err) done(err);
+
+					db.collection.getProperties(ret.id,function(err,ret){
 						done(err);
 					});
 				});
