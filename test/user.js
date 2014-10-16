92b094b46536403c2c6ecd2941d02d625b435745
Anders Elo <anders@kaerus.com>
2014-10-17 01:40:03 +0200

    fixed user tests [ci skip]

--- test/user.js
+++ test/user.js
@@ -6,29 +6,18 @@ try {
     arango = require('..')
 }
 
-function check(done, f) {
-    try {
-	f()
-	done()
-    } catch (e) {
-	console.log(e);
-	done(e)
-    }
-}
-
-
 describe("user", function () {
 
     before(function (done) {
 	
 	db = arango.Connection("/_system");
-	db.database.delete("newDatabase", function (err, ret) {
-	    db.database.create("newDatabase", function (err, ret) {
+	
+	db.database.delete("newDatabase").end( function () {
+	    db.database.create("newDatabase").then( function() {
 		db = db.use('/newDatabase');
-		done();
-	    });
+	    }).callback(done);
 	});
-
+	
     })
 
     it('create a user', function (done) {
@@ -36,12 +25,10 @@ describe("user", function () {
 	db.user.create("hans", "passwordHans", true, {
 	    "vorname": "hans",
 	    "nachname": "otto"
-	}, function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(false);
-		message.status.should.equal(201);
-	    });
-	});
+	}).then( function (ret) {
+	    ret.error.should.equal(false);
+	    ret.code.should.equal(201);
+	}).callback(done);
     })
 
     it('create a user', function (done) {
@@ -49,12 +36,10 @@ describe("user", function () {
 	db.user.create("hans2", "passwordHans2", true, {
 	    "vorname": "hans2",
 	    "nachname": "otto2"
-	}, function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(false);
-		message.status.should.equal(201);
-	    });
-	});
+	}).then( function (ret) {
+	    ret.error.should.equal(false);
+	    ret.code.should.equal(201);
+	}).callback(done);
     })
 
     it('create an already existing user', function (done) {
@@ -62,118 +47,116 @@ describe("user", function () {
 	db.user.create("hans", "passwordHans", true, {
 	    "vorname": "hans",
 	    "nachname": "otto"
-	}, function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(true);
-		message.status.should.equal(400);
-	    });
+	}).catch( function (err) {
+	    err.error.should.equal(true);
+	    err.code.should.equal(400);
+	    done();
 	});
     })
 
     it('get user', function (done) {
 	
-	db.user.get("hans", function (err, ret, message) {
-	    check(done, function () {
+	db.user.get("hans")
+	    .then( function (ret) {
 		ret.error.should.equal(false);
-		message.status.should.equal(200);
-	    });
-	});
+		ret.code.should.equal(200);
+	    }).callback(done);
     })
     it('get non existing user', function (done) {
 	
-	db.user.get("hansGibtsNicht", function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(true);
-		message.status.should.equal(404);
+	db.user.get("hansGibtsNicht")
+	    .catch(function (err) {
+		err.error.should.equal(true);
+		err.code.should.equal(404);
+		done();
 	    });
-	});
     })
 
     it('delete user', function (done) {
 	
-	db.user.delete("hans2", function (err, ret, message) {
-	    check(done, function () {
+	db.user.delete("hans2")
+	    .then(function (ret) {
+		
 		ret.error.should.equal(false);
-		message.status.should.equal(202);
-	    });
-	});
+		ret.code.should.equal(202);
+	    }).callback(done);
     })
     it('delete non existing user', function (done) {
 	
-	db.user.delete("hansGibtsNicht", function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(true);
-		message.status.should.equal(404);
+	db.user.delete("hansGibtsNicht")
+	    .catch(function (err) {
+		err.error.should.equal(true);
+		err.code.should.equal(404);
+		done();
 	    });
-	});
     })
 
     it('patch non existing user', function (done) {
 	
-	db.user.patch("hans2", "newPassword", function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(true);
-		message.status.should.equal(404);
+	db.user.patch("hans2", "newPassword")
+	    .catch(function (err) {
+		err.error.should.equal(true);
+		err.code.should.equal(404);
+		done();
 	    });
-	});
     })
+    
     it('patch user', function (done) {
 	
 	db.user.patch("hans", "newPassword", false, {
 	    "nachname": "otto-müller",
 	    "married": true
-	}, function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(false);
-		message.status.should.equal(200);
-	    });
-	});
+	}).then( function (ret) {
+	    ret.error.should.equal(false);
+	    ret.code.should.equal(200);
+	}).callback(done);
     })
+    
     it('get user to verify last patch', function (done) {
 	
-	db.user.get("hans", function (err, ret, message) {
-	    check(done, function () {
+	db.user.get("hans")
+	    .then(function (ret) {
+
 		ret.error.should.equal(false);
 		ret.extra.should.have.property("nachname");
 		ret.extra.should.have.property("married");
 		ret.extra.should.have.property("vorname");
-		message.status.should.equal(200);
-	    });
-	});
+		ret.code.should.equal(200);
+	    }).callback(done);
     })
 
     it('put non existing user', function (done) {
 	
-	db.user.put("hans2", "newPassword", function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(true);
-		message.status.should.equal(404);
+	db.user.put("hans2", "newPassword")
+	    .catch(function (err) {
+		err.error.should.equal(true);
+		err.code.should.equal(404);
+		done();
 	    });
-	});
     })
+    
     it('put user', function (done) {
 	
 	db.user.put("hans", "newPassword", false, {
 	    "married": false,
 	    "sad": true
-	}, function (err, ret, message) {
-	    check(done, function () {
-		ret.error.should.equal(false);
-		message.status.should.equal(200);
-	    });
-	});
+	}).then( function (ret) {
+	    ret.error.should.equal(false);
+	    ret.code.should.equal(200);
+	}).callback(done);
     })
+    
     it('get user to verify last put', function (done) {
 	
-	db.user.get("hans", function (err, ret, message) {
-	    check(done, function () {
+	db.user.get("hans")
+	    .then(function (ret) {
+		
 		ret.extra.should.not.have.property("nachname");
 		ret.extra.should.have.property("married");
 		ret.extra.should.have.property("sad");
 		ret.extra.should.not.have.property("vorname");
 		ret.error.should.equal(false);
-		message.status.should.equal(200);
-	    });
-	});
+		ret.code.should.equal(200);
+	    }).callback(done);
     })
 })
